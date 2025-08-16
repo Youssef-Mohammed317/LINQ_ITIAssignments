2db1710b0edd2e687b3613fb38cde5f1ba5bf437
Youssef-Mohammed317 <youssefmohammad040317@gmail.com>
2025-08-17 01:20:26 +0300

    End of Demo of Day01

---
+++ Demo Of Day1/Extensions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Of_Day1
+{
+    internal static class Extensions
+    {
+        public static IEnumerable<T> Filter<T>(this List<T> list, Predicate<T> predicate)
+        {
+            //List<T> result = new List<T>();
+            foreach (var item in list)
+            {
+                if (predicate.Invoke(item))
+                    //result.Add(item);
+                    yield return item; // using yield return to create an iterator
+            }
+            //return result;
+        }
+
+        public static string RemoveVowels(this string str)
+        {
+            var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+            foreach(char c in str)
+            {
+                if (vowels.Contains(c))
+                {
+                    str = str.Replace(c.ToString(), string.Empty);
+                }
+            }
+            return str;
+        }
+
+        public static char GetMiddleCharacter(this string str)
+        {
+            return str.Length % 2 == 0 
+                ? str[str.Length / 2 - 1] 
+                : str[str.Length / 2];
+        }
+
+        public static void PrintMe(this char c)
+        {
+            Console.WriteLine($"Character: {c}");
+        }
+
+        public static IEnumerable<int> Sequence()
+        {
+            // sub routine
+            //List<int> list = new List<int> { 1, 2, 3 };
+            //return list;
+
+            // ko routine
+            Console.WriteLine("Return 1...........");
+            yield return 1;
+
+            Console.WriteLine("Return 2...........");
+            yield return 2;
+
+            Console.WriteLine("Return 3...........");
+            yield return 3;
+        }
+
+    }
+}
--- Demo Of Day1/Program.cs
+++ Demo Of Day1/Program.cs
@@ -2,9 +2,116 @@
 {
     internal class Program
     {
+
+        static public List<T> Filter<T>(List<T> list,Predicate<T> predicate)
+        {
+            List<T> result = new List<T>();
+            foreach (var item in list)
+            {
+                if(predicate.Invoke(item)) 
+                    result.Add(item);
+            }
+            return result;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Implicitly typed lacal variable (var) 
+            //when you don't want to specify the type at the left side
+
+            //var number = 10; // variable type is inferred as int
+            //var price = 19.99; // variable type is inferred as double
+            //var num = 5.5f; // variable type is inferred as float
+            //var name = "John"; // variable type is inferred as string
+            //var isActive = true; // variable type is inferred as bool
+            //var numbers = new[] { 1, 2, 3, 4, 5 }; // variable type is inferred as int[]
+
+            //number = "Hello"; // Error: Cannot implicitly convert type 'string' to 'int'
+
+            //Dictionary<string,List<Employee>> employees = new Dictionary<string, List<Employee>>();
+            //var employees = new Dictionary<string, List<Employee>>();
+
+            #endregion
+
+            #region Anonymous Objects 
+
+            // you don't know the type at compile time
+            //var obj = new { Name = "John", Age = 30 }; // Anonymous type with properties Name and Age
+            //var e = new { Name = "Jone", Age = 25 };
+
+            //obj = e; // This is valid because both obj and e are of the same anonymous type
+
+            //var ee = new { Name = "Jone", Age = 25, Address = "123 Main St" }; // This is a different anonymous type
+
+            //// obj = ee; // Error: Cannot implicitly convert type 'anonymous type with 3 properties' to 'anonymous type with 2 properties'
+
+            #endregion
+
+            #region Extension Methods
+
+            //var list = new List<int> { 1, 2, -3, 4, 5, 6, -7, 8, 9 };
+
+            //Filter(list,c => c > 0); // old way
+
+            //list.Filter(c => c > 0); // new way
+
+            //string str = "Hello World!";
+            //string msg = str.RemoveVowels();
+            //Console.WriteLine(msg);
+
+            //string str = "Hello World!";
+
+            //string msg = str.RemoveVowels();
+
+            //char middleChar = msg.GetMiddleCharacter();
+
+            //middleChar.PrintMe(); // This will print the middle character of the string without vowels
+
+            //str.RemoveVowels().GetMiddleCharacter().PrintMe(); // Chaining methods
+
+            #endregion
+
+            #region Ko Routine vs Subroutine
+
+            //IEnumerable<int> list = Extensions.Sequence();
+
+            //foreach (var item in list)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //var list = new List<int> { 1, 2, -3, 4, 5, 6, -7, 8, 9 };
+
+            //IEnumerable<int> newList = list.Filter(c => c > 0);
+
+            //// new list is an iterator, it will not execute until you iterate over it
+
+            //foreach (var item in newList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            //var list = new List<int> { 1, 2, -3, 4, 5, 6, -7, 8, 9 };
+
+            //IEnumerable<int> newList = list.Filter(c => c % 2 == 0);
+
+            //foreach (var item in newList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Console.WriteLine("---------------------");
+
+            //list[0] = 10; // subroutine no change in the newList 
+            //              // ko routine change in the newList
+
+            //foreach (var item in newList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion
         }
     }
 }
